cf20c5ade05ce6e82343c57f1a2bf61e653f0f73
sivog <sivograkovkirill@mail.ru>
2024-12-01 00:49:59 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
-import { App } from './App';
+import { App } from '@components/application';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-   <BrowserRouter>
-      <React.StrictMode>
-         <App />
-      </React.StrictMode>
-   </BrowserRouter>,
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
 );
