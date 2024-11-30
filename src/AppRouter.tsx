7f4d1d8482e881e5466441eff9f3014e33a54d74
sivog <sivograkovkirill@mail.ru>
2024-12-01 00:39:04 +0300

    init project

---
+++ src/AppRouter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { HomePage } from './components/pages/MainPage';
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+        </Routes>
+    );
+};
