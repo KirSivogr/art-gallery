eff3e5f91c6b9ed7e95f4c69a25fa748412fd9cd
sivog <sivograkovkirill@mail.ru>
2024-12-01 00:44:25 +0300

    add constants colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+   gradient: 'linear-gradient(90deg, #343333 38.05%, #484848 69.22%, #282828 98.98%)',
+   green: '#4AA851',
+   white: '#ffffff',
+   grey: '#3939390d',
+};
