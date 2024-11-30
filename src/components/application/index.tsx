eec801e67d78d780ef97e0d3e86df0f61c948cf9
sivog <sivograkovkirill@mail.ru>
2024-12-01 02:30:55 +0300

    add app file

---
+++ src/components/application/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from '@components/pages/MainPage';
+
+export const App = () => {
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<HomePage />} />
+         </Routes>
+      </BrowserRouter>
+   );
+};
