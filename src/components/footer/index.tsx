0f0254403b625385c519ac3613e13771c4878e92
sivog <sivograkovkirill@mail.ru>
2024-12-01 00:40:38 +0300

    add footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Wrapper } from './styled';
+
+export const Footer = () => {
+   return (
+      <Wrapper>
+         <Container />
+      </Wrapper>
+   );
+};
---
+++ src/components/footer/styled.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+import { colors } from '@/constants/colors';
+
+export const Container = styled.div`
+   width: 70%;
+   margin: 0 auto;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   height: 63px;
+`;
+
+export const Wrapper = styled.div`
+   display: flex;
+   align-items: center;
+   height: 127px;
+   width: 100%;
+   background: ${colors.white};
+`;
+
+export const Modsen = styled.img`
+   width: 164px;
+   height: 60px;
+`;
