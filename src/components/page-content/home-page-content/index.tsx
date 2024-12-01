9769b9891e25b7f13a3111de7dc963b33f0dc796
sivog <sivograkovkirill@mail.ru>
2024-12-01 14:26:39 +0300

    add gallery

--- src/components/page-content/home-page-content/index.tsx
+++ src/components/page-content/home-page-content/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ArtCard } from '@components/cards-components/art-card';
+import { SubtitleBlock } from '@components/subtitle-block';
+import { ErrorMessage, Formik } from 'formik';
+import searchIconDefault from 'src/assets/searchIcon.svg';
+import searchIconGreen from 'src/assets/searchIconGreen.svg';
 
-import { Container, Title, TitleBlock, Wrapper } from './styled';
+import { useGetArtList } from '@/hooks/useGetArtList';
+
+import { validationSchema } from './schema';
+import {
+   Container,
+   Gallery,
+   Input,
+   SearchButton,
+   StyledForm,
+   Title,
+   TitleBlock,
+   Wrapper,
+} from './styled';
 
 export const HomePageContent = () => {
+   const [query, setQuery] = useState('');
+   const [currentPage, setCurrentPage] = useState(1);
+   const { artList, isArtListLoading, totalPages, isError, setArtList } = useGetArtList(
+      query,
+      currentPage,
+   );
+   const navigate = useNavigate();
+
    return (
       <Wrapper>
          <Container>
@@ -12,6 +38,53 @@ export const HomePageContent = () => {
                </Title>
                <Title>Here!</Title>
             </TitleBlock>
+            <Formik
+               initialValues={{ searchQuery: '' }}
+               validationSchema={validationSchema}
+               onSubmit={(values, { resetForm }) => {
+                  setQuery(values.searchQuery);
+                  resetForm();
+               }}
+            >
+               {({ handleSubmit, values, handleChange }) => (
+                  <StyledForm onSubmit={handleSubmit}>
+                     <StyledForm>
+                        <Input
+                           name='searchQuery'
+                           placeholder='Search Art, Artist, Work...'
+                           value={values.searchQuery}
+                           onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                              handleChange(event);
+                              // handleSearchDebounce(event.target.value);
+                           }}
+                        />
+                        <SearchButton
+                           backgroundImageDefault={searchIconDefault}
+                           backgroundImageGreen={searchIconGreen}
+                           type='submit'
+                        />
+                     </StyledForm>
+                     <ErrorMessage name='search' component='div' className='error' />
+                  </StyledForm>
+               )}
+            </Formik>
+            <SubtitleBlock
+               infoTitle='Topics for you'
+               infoSubtitle='Our special gallery'
+            />
+            <Gallery>
+               {artList.map(art => (
+                  <ArtCard
+                     imageUrl={art.imageUrl}
+                     title={art.title}
+                     artist_title={art.artist_title}
+                     key={art.id}
+                     id={art.id}
+                     isLoading={isArtListLoading}
+                     onClick={() => navigate(`artinfo/${art.id}`, { replace: false })}
+                  />
+               ))}
+            </Gallery>
          </Container>
       </Wrapper>
    );
--- src/components/page-content/home-page-content/styled.ts
+++ src/components/page-content/home-page-content/styled.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Field, Form } from 'formik';
 
 import { colors } from '@/constants/colors';
 
@@ -29,5 +30,78 @@ export const Title = styled.div`
    span {
       font-weight: bold;
       color: ${colors.green};
+   }
+`;
+
+export const StyledForm = styled(Form)`
+   display: flex;
+   position: relative;
+   justify-content: center;
+   padding: 16px;
+   @media (max-width: 390px) {
+      margin-bottom: 10px;
+   }
+   @media (min-width: 391px) and (max-width: 768px) {
+      margin-bottom: 10px;
+   }
+   @media (min-width: 769px) {
+      margin-bottom: 10px;
+   }
+`;
+
+export const Input = styled(Field)`
+   display: flex;
+   padding: 10px;
+   height: 50px;
+   border: none;
+   border-radius: 20px;
+   margin-bottom: 10px;
+   background: ${colors.white};
+   font-family: 'Lexend Deca', sans-serif;
+   @media (max-width: 390px) {
+      width: 200px;
+   }
+   @media (min-width: 391px) and (max-width: 768px) {
+      width: 342px;
+   }
+   @media (min-width: 769px) {
+      width: 542px;
+   }
+`;
+
+export const SearchButton = styled.button<{
+   backgroundImageDefault: string;
+   backgroundImageGreen: string;
+}>`
+   position: absolute;
+   width: 32px;
+   height: 32px;
+   background-image: url(${({ backgroundImageDefault }) => backgroundImageDefault});
+   background-size: cover;
+   background-color: ${colors.white};
+   border: none;
+   right: 35px;
+   top: 35px;
+   cursor: pointer;
+   transition: background 0.3s ease;
+   &:hover {
+      background-image: url(${({ backgroundImageGreen }) => backgroundImageGreen});
+   }
+`;
+
+export const Gallery = styled.div`
+   display: grid;
+   grid-template-columns: repeat(3, 1fr);
+   gap: 20px;
+   flex-wrap: wrap;
+   margin-bottom: 120px;
+   @media (min-width: 769px) and (max-width: 1280px) {
+      grid-template-columns: repeat(2, 1fr);
+      margin-bottom: 80px;
+   }
+   @media (max-width: 769px) {
+      grid-template-columns: repeat(1, 1fr);
+      margin-bottom: 80px;
+      gap: 10px;
    }
 `;
