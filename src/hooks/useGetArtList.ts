b80700d5f6029c674cf7af2067c293a2d73dfb15
sivog <sivograkovkirill@mail.ru>
2024-12-01 14:25:29 +0300

    add custom hook

---
+++ src/hooks/useGetArtList.ts
@@ -0,0 +1,50 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+import { getArtLists } from '@/api/api';
+import { ArtByIdWithImage } from '@/types/interfaces';
+
+interface UseFetchArtListProps {
+   artList: ArtByIdWithImage[];
+   totalPages: number;
+   isArtListLoading: boolean;
+   isError: boolean;
+   setArtList: Dispatch<SetStateAction<ArtByIdWithImage[]>>;
+}
+
+export function useGetArtList(
+   query: string | undefined,
+   pageNumber: number,
+): UseFetchArtListProps {
+   const [artList, setArtList] = useState<ArtByIdWithImage[]>([]);
+   const [isArtListLoading, setIsArtListLoading] = useState(false);
+   const [isError, setIsError] = useState(false);
+   const [totalPages, setTotalPages] = useState(0);
+
+   useEffect(() => {
+      const fetchArtList = async () => {
+         setIsArtListLoading(true);
+         try {
+            const [res, pagination] = await getArtLists(query, pageNumber);
+
+            const artsWithImages = res.map(
+               (data: ArtByIdWithImage) =>
+                  ({
+                     ...data,
+                     imageUrl: `https://www.artic.edu/iiif/2/${data.image_id}/full/387,/0/default.jpg`,
+                  } as ArtByIdWithImage),
+            );
+
+            setArtList(artsWithImages);
+            setTotalPages(pagination.total_pages);
+         } catch (error) {
+            setIsError(true);
+         } finally {
+            setIsArtListLoading(false);
+         }
+      };
+
+      fetchArtList();
+   }, [query, pageNumber]);
+
+   return { artList, isArtListLoading, isError, totalPages, setArtList };
+}
