1a5deb6de71fc878f1e9a2bb83e8f96a95f9e2eb
sivog <sivograkovkirill@mail.ru>
2024-12-01 14:23:54 +0300

    add new color

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,6 +1,7 @@
 export const colors = {
    gradient: 'linear-gradient(90deg, #343333 38.05%, #484848 69.22%, #282828 98.98%)',
    green: '#4AA851',
+   primary: 'rgb(57, 57, 57)',
    white: '#ffffff',
    grey: '#3939390d',
 };
