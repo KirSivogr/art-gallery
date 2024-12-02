7b5659a96d821327cb9a666f45082dac2366ce0f
sivog <sivograkovkirill@mail.ru>
2024-12-02 16:36:58 +0300

    add error component

---
+++ src/components/error-component/index.tsx
@@ -0,0 +1,5 @@
+import { ErrorMessage } from './styled';
+
+export const ErrorComponent = () => {
+   return <ErrorMessage>Something went wrong, please reload page</ErrorMessage>;
+};
---
+++ src/components/error-component/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ErrorMessage = styled.div`
+   padding: 150px;
+   text-align: center;
+   font-size: 16px;
+
+   @media (max-width: 760px) {
+      padding: 50px;
+      font-size: 16px;
+   }
+`;
