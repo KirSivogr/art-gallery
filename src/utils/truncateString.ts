687191eb552e86aa963eea185f9cdf59267738e3
sivog <sivograkovkirill@mail.ru>
2024-12-02 16:37:15 +0300

    fix util

--- src/utils/truncateString.ts
+++ src/utils/truncateString.ts
@@ -1,9 +1,13 @@
 import { LENGTH_OF_TRUNCATE } from '@/constants/constants';
 
-export const truncateString = (str: string) => {
-   if (str.length <= LENGTH_OF_TRUNCATE) {
+export const truncateString = (str: string | null) => {
+   if (!str) {
+      return;
+   }
+
+   if (str?.length <= LENGTH_OF_TRUNCATE) {
       return str;
    }
 
-   return `${str.slice(0, LENGTH_OF_TRUNCATE)}...`;
+   return `${str?.slice(0, LENGTH_OF_TRUNCATE)}...`;
 };
